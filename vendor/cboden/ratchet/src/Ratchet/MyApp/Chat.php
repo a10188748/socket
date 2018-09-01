34748fb6687ebb92239867a00fde256adb0a1234
sean_yeh <sean_yeh@hiiir.com>
2018-09-01 13:27:21 +0800

    1

--- vendor/cboden/ratchet/src/Ratchet/MyApp/Chat.php
+++ vendor/cboden/ratchet/src/Ratchet/MyApp/Chat.php
@@ -13,6 +13,7 @@ class Chat implements MessageComponentInterface {
         $this->clients = array();
         $this->message = array();
         session_start();
+        
     }
  
     public function onOpen(ConnectionInterface $conn) {
